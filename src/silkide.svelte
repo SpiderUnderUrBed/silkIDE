13d7b6add678c3e4da8c822274bba8f9662bee9d
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-05-05 21:10:21 +1200

    I have done quite a lot of design on the IDE mainly adding file explorer and a textarea to type code

--- src/silkide.svelte
+++ src/silkide.svelte
@@ -2,18 +2,27 @@
     
 </script>
 
+<style>
+    @import 'CSS/stylesheet_for_ide.css';
+</style>
+
 <main>
     <div class="container">
-        <div class="left-tab">
-
-        </div>
-
         <div class="file-explorer">
+            <div class="header-file-explorer">
+                <center><p>File Explorer:</p></center>
+            </div>
 
+            <div class="files-folders-handler">
+                <!-- ADD THE FILE ORGANIZER STUFF HERE @SPIDER -->
+            </div>
         </div>
 
-        <div class="">
-
+        <div class="csharp-edit">
+            <div>
+                <!-- MAKE SURE TO ADD THE VERTICAL LINE LIKE IN VSCODE -->
+                <textarea class="edit"></textarea>
+            </div>
         </div>
     </div>
 </main>
